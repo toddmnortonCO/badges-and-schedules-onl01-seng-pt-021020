829100c7bb90b3e88a81929550241b629a3d7182
Todd Norton <toddmnorton@live.com>
2020-02-14 21:06:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers.map do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map do |name, index| # inside the pipes can only be one argument, unless on a hash
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers)
+  assign_rooms(speakers)
+end
+
+printer(speakers)
